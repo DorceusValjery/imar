e0151962a5e77e96e60a53a2e97e65779a2e4293
Dorceus Valjery Welinghton <welinghton.dorceus@etu.univ-orleans.fr>
2019-05-18 11:01:55 +0200

    adding Person domain

--- grails-app/domain/imar/utils/Person.groovy
+++ grails-app/domain/imar/utils/Person.groovy
@@ -1,13 +1,18 @@
-package imar
+package imar.utils
 
 class Person {
 
     String firstname;
     String lastname;
     String secondName;
-    Date dateOfBirth;
+    Date dateOfBirth = new Date()
     String email;
 
     static constraints = {
+        email email:true, nullable: true
+        dateOfBirth nullable: true
+        secondName nullable: true
+        lastname blank: false
+        firstname blank: false
     }
 }
