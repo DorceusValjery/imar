eb6b5500e340b41c26a16f8c353e995cfb4b2f2e
Dorceus Valjery Welinghton <welinghton.dorceus@etu.univ-orleans.fr>
2019-05-18 11:11:46 +0200

    adding view assets such as javascripts and stylesheets

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,7 +5,16 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-3.3.1.min
-//= require bootstrap
-//= require popper.min
+//= require vendors/base/vendor.bundle.base.js
+//= require vendors/chart.js/Chart.min.js
+//= rewuire vendors/datatables.net/jquery.dataTables.js
+//= require vendors/datatables.net-bs4/dataTables.bootstrap4.js
+//= require js/off-canvas.js
+//= require js/hoverable-collapse.js
+//= require js/template.js
+//= require js/dashboard.js
+//= require js/data-table.js
+//= require js/jquery.dataTables.js
+//= require js/dataTables.bootstrap4.js
 //= require_self
+
