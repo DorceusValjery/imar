7f4693fd8789386e7d150140da52a3a60383dae5
Dorceus Valjery Welinghton <welinghton.dorceus@etu.univ-orleans.fr>
2019-05-18 10:59:31 +0200

    adding Prospect domain

--- grails-app/domain/imar/order/Order.groovy
+++ grails-app/domain/imar/order/Order.groovy
@@ -1,10 +1,25 @@
-package imar
+package imar.order
+import imar.client.Client
 
 class Order {
 
+    String orderNumber;
+    Date placedOn;
+    String termsOfPayment;
+    Date payedOn;
+    String status;
+    Date deliveredOn;
+    Port portOfLoading;
+    enum shippingMethod{ AIR('air'),SEA('sea'),ROAD('road')}
+    enum typeOfCargo{ FCL('FCL'), LCL('LCL'), CONSOLIDATED('CONSOLIDATED')}
+    String shippingStatus;
+    String deliveryStatus;
+    Currency totalCostOfDelivery;
+    String pricingTerms;
 
-    static hasOne = [client:Client]
-    static hasMany = [items:Item]
+
+    static hasOne = [client: Client]
+    static hasMany = [products:Product]
     static constraints = {
     }
 }
